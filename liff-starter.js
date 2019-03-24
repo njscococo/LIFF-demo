9968217be7e85f1583deb9174b5984bc93ca8405
njscococo <njscococo@gmail.com>
2019-03-24 16:59:00 +0800

    liff testing

--- liff-starter.js
+++ liff-starter.js
@@ -88,8 +88,8 @@ function initializeApp(data) {
                         },
 
                         {
-                            "type": "image",
-                            "originalContentUrl": `https://linetestingserver.herokuapp.com/user/${data.context.userId}/${res.data.id}`,
+                            type: "image",
+                            originalContentUrl: `https://linetestingserver.herokuapp.com/user/${data.context.userId}/${res.data.id}`,
                             // "size": "full",
                             // "aspectRatio": "1.91:1"
                         }
